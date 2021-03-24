7fb3bf7460eac1dcea71f75ee79c86a36bd9868c
zsszakal <zs.szakal@outlook.com>
2021-03-24 17:02:44 +0100

    Implement class, modify pom.xml

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -4,17 +4,49 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessResults {
+    public static class Competitors {
+        private String name;
+        private int sum;
 
+        public Competitors(String name, int sum) {
+            this.name = name;
+            this.sum = sum;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSum() {
+            return sum;
+        }
+    }
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        List<Competitors> competitorsList = new ArrayList<>();
+        try {
+            List<String> data = Files.readAllLines(Paths.get(fileName));
+            for (int i = 0; i < data.size(); i++) {
+                String[] competitorData = data.get(i).split(",");
+                competitorsList.add(new Competitors(competitorData[0],
+                        Integer.parseInt(competitorData[1]) +
+                                Integer.parseInt(competitorData[2]) +
+                                Integer.parseInt(competitorData[3]) +
+                                Integer.parseInt(competitorData[4]) +
+                                Integer.parseInt(competitorData[5])));
+            }
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+        return competitorsList.stream()
+                .sorted((Comparator.comparingInt(Competitors::getSum).reversed()))
+                .map(Competitors::getName)
+                .collect(Collectors.toList());
     }
 
 }
